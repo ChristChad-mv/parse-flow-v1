019616c002450f58eb335e4565b581ef636823fd
Christ Chadrak MVOUNGOU <ccmvoungou@gmail.com>
2025-03-08 02:05:04 +0100

    add feature : All about workflows  (pages, helper and components)

---
+++ app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
+import React, { useState } from 'react'
+
+function CreateWorkflowDialog({ triggerText }: { triggerText?: string }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button> {triggerText ?? "Create workflow"} </Button>
+      </DialogTrigger>
+      
+    </Dialog>
+  )
+}
+
+export default CreateWorkflowDialog
