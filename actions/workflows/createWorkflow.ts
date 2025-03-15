3b3a704fa8c8d574dbc993b18e0bff24c31622ae
Christ Chadrak MVOUNGOU <ccmvoungou@gmail.com>
2025-03-15 14:15:53 +0100

    workflow add : creation of workflow

---
+++ actions/workflows/createWorkflow.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { createWorkflowSchema, createWorkflowSchemaType } from "@/schema/workflow";
+import { auth } from "@clerk/nextjs/server";
+import { WorkflowStatus } from "@/types/workflow";
+
+export async function CreateWorkflow(form: createWorkflowSchemaType) {
+
+  const { success, data } = createWorkflowSchema.safeParse(form);
+  if (!success) {
+    throw new Error("Invalid form data");
+  }
+
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Unauthenticated");
+  }
+
+  const result = await prisma.workflow.create({
+    data: {
+      userId: userId,
+      status: WorkflowStatus.DRAFT,
+      definition: "TODO", 
+      description: "",
+      ...data,
+    },
+  });
+
+  if (!result) {
+    throw new Error("Failed to create a workflow");
+  }
+
+  return { id: result.id };
+}
