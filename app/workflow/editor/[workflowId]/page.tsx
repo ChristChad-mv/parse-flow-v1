7ad4b5fb26c9a072f8fa1aeae154e801d1c6cfc3
Christ Chadrak MVOUNGOU <ccmvoungou@gmail.com>
2025-04-15 19:28:18 +0200

    add features: Implement page component for workflow editor with authentication and workflow retrieval

---
+++ app/workflow/editor/[workflowId]/page.tsx
@@ -0,0 +1,23 @@
+import { prisma } from '@/lib/prisma';
+import { auth } from '@clerk/nextjs/server';
+import React from 'react'
+import Editor from '../../_components/Editor';
+
+async function page({ params }: { params:  { workflowId : string} }) {
+  const { workflowId } = await params;
+  const { userId } = await auth();
+
+  if(!userId) return <div>Unauthentificated </div>
+  const workflow = await prisma.workflow.findUnique({
+    where: {
+      id: workflowId, 
+      userId,
+    }
+  })
+
+  if(!workflow) return <div>Workflow not found</div>
+
+  return <Editor workflow={workflow} />
+}
+
+export default page
