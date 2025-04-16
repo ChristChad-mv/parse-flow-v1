c059022125757f62d269c85f4f7f46c9a2c94b08
Christ Chadrak MVOUNGOU <ccmvoungou@gmail.com>
2025-04-16 20:48:48 +0200

    add features: Implement NodeParamField and StringParam components for dynamic task parameter handling

---
+++ app/workflow/_components/nodes/param/StringParam.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React, { useId, useState } from 'react'
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { ParamProps } from '@/types/appNode';
+
+function StringParam({ param, value, updateNodeParamValue }: ParamProps ) {
+  const [internalValue, setInternalValue] = useState(value);
+  const { id } = useId();
+  return (
+    <div className='space-y-1 p-1 w-full'>
+      <Label htmlFor={id} className='text-xs flex'>
+        {param.name}
+        {param.required && <p className='text-red-500 px-1'>*</p> }
+      </Label>
+      <Input 
+        id={id}
+        className='text-xs'
+        value={value}
+        placeholder='Enter a value here'
+        onChange={(e) => setInternalValue(e.target.value)}
+        onBlur={(e) => updateNodeParamValue(e.target.value)}  
+      />
+      {param.helperText && (
+        <p className='text-muted-foreground px-2'>{param.helperText}</p>
+      )}
+    </div>
+  )
+}
+
+export default StringParam
