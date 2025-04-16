ef5e3552ac4962d8c85093baf0e8f90eeb1c4dbb
Christ Chadrak MVOUNGOU <ccmvoungou@gmail.com>
2025-04-16 20:49:04 +0200

    add features: Initialize workflow creation with dynamic flow structure and launch browser task

--- actions/workflows/createWorkflow.ts
+++ actions/workflows/createWorkflow.ts
@@ -4,6 +4,10 @@ import { prisma } from "@/lib/prisma";
 import { createWorkflowSchema, createWorkflowSchemaType } from "@/schema/workflow";
 import { auth } from "@clerk/nextjs/server";
 import { WorkflowStatus } from "@/types/workflow";
+import { AppNode } from "@/types/appNode";
+import { Edge } from "@xyflow/react";
+import { CreateFlowNode } from "@/lib/workflow/createFlowNode";
+import { TaskType } from "@/types/task";
 
 export async function CreateWorkflow(form: createWorkflowSchemaType) {
 
@@ -17,12 +21,19 @@ export async function CreateWorkflow(form: createWorkflowSchemaType) {
     throw new Error("Unauthenticated");
   }
 
+  const initialFlow: { nodes: AppNode[]; edges: Edge[] } = {
+    nodes: [], 
+    edges: [],
+  };
+
+  initialFlow.nodes.push(CreateFlowNode(TaskType.LAUNCH_BROWSER));
+
   const result = await prisma.workflow.create({
     data: {
       ...data, 
       userId,
       status: WorkflowStatus.DRAFT,
-      definition: data.definition ?? "", 
+      definition: JSON.stringify(initialFlow), 
       description: data.description ?? "",
     },
   });
