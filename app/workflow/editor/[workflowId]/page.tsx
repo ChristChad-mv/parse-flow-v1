4545965af8edd83298e1e3a1fa3ee96aa4727a92
Christ Chadrak MVOUNGOU <ccmvoungou@gmail.com>
2025-04-17 21:45:47 +0200

    refactor: Rename page function to Page and improve type definitions for clarity

--- app/workflow/editor/[workflowId]/page.tsx
+++ app/workflow/editor/[workflowId]/page.tsx
@@ -1,23 +1,30 @@
 import { prisma } from '@/lib/prisma';
 import { auth } from '@clerk/nextjs/server';
-import React from 'react'
+import React from 'react';
 import Editor from '../../_components/Editor';
+import { NextPage } from 'next';
 
-async function page({ params }: { params:  { workflowId : string} }) {
-  const { workflowId } = await params;
-  const { userId } = await auth();
-
-  if(!userId) return <div>Unauthentificated </div>
-  const workflow = await prisma.workflow.findUnique({
-    where: {
-      id: workflowId, 
-      userId,
-    }
-  })
-
-  if(!workflow) return <div>Workflow not found</div>
-
-  return <Editor workflow={workflow} />
+interface WorkflowPageProps {
+  params: {
+    workflowId: string;
+  };
 }
 
-export default page
+const Page: NextPage<WorkflowPageProps> = async ({ params }) => {
+  const { workflowId } = params;
+  const { userId } = await auth();
+
+  if (!userId) return <div>Unauthentificated </div>;
+  const workflow = await prisma.workflow.findUnique({
+    where: {
+      id: workflowId,
+      userId,
+    },
+  });
+
+  if (!workflow) return <div>Workflow not found</div>;
+
+  return <Editor workflow={workflow} />;
+};
+
+export default Page;
