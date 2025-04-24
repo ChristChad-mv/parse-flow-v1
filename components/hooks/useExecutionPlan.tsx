09160536144b694d3424b9146763d0ee95f85fef
Christ Chadrak MVOUNGOU <ccmvoungou@gmail.com>
2025-04-24 17:41:52 +0200

    fix: Refine error handling in useExecutionPlan hook and improve type definitions

--- components/hooks/useExecutionPlan.tsx
+++ components/hooks/useExecutionPlan.tsx
@@ -2,29 +2,33 @@ import { useReactFlow } from "@xyflow/react";
 import { useCallback } from "react";
 
 import { FlowToExecutionPlan, FlowToExecutionPlanValidationError } from "@/lib/workflow/executionPlan";
-import { AppNode } from "@/types/appNode";
+import { AppNode, AppNodeMissingInputs } from "@/types/appNode";
 import useFlowValidation from "./useFlowValidation";
 import { toast } from "sonner";
+
+type ExecutionPlanError = {
+  type: FlowToExecutionPlanValidationError;
+  invalidElements?: AppNodeMissingInputs[];
+}
 
 const useExecutionPlan = () => {
   const { toObject } = useReactFlow();
   const { setInvalidInputs, clearErrors } = useFlowValidation();
 
-  const handleError = useCallback((error:any) => {
+  const handleError = useCallback((error: ExecutionPlanError) => {
     switch (error.type) {
       case FlowToExecutionPlanValidationError.NO_ENTRY_POINT:
         toast.error("No entry point found");
         break;
       case FlowToExecutionPlanValidationError.INVALID_INPUTS:
         toast.error("Not all inputs values are set");
-        setInvalidInputs(error.invalidElements);
+        setInvalidInputs(error.invalidElements ?? []);
         break;
       default:
         toast.error("something went wrong");
         break;
     }
   }, [setInvalidInputs])
-
 
   const generateExecutionPlan = useCallback(() => {
     const { nodes, edges } = toObject();
