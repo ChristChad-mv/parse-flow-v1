d9d8b74d6d2af03b492198d1e8538544832663f3
Christ Chadrak MVOUNGOU <ccmvoungou@gmail.com>
2025-04-24 17:36:33 +0200

    feat: Implement workflow execution creation in RunWorkflow function

--- actions/workflows/runWorkflow.ts
+++ actions/workflows/runWorkflow.ts
@@ -2,6 +2,7 @@
 
 import { prisma } from "@/lib/prisma";
 import { FlowToExecutionPlan } from "@/lib/workflow/executionPlan";
+import { TaskRegistry } from "@/lib/workflow/task/registry";
 import { WorkflowExecutionPlan } from "@/types/workflow";
 import { auth } from "@clerk/nextjs/server";
 
@@ -40,4 +41,35 @@ export async function RunWorkflow(form: {
 
   executionPlan = result.executionPlan;
   console.log(executionPlan);
+
+  const execution = await prisma.workflowExecution.create({
+    data: {
+      workflowId: form.workflowId, 
+      userId, 
+      status: "PENDING", 
+      startedAt: new Date(), 
+      trigger: "manuel", 
+      phases: {
+        create: executionPlan.flatMap((phase) => {
+          return phase.nodes.flatMap((node) => {
+            return {
+              userId, 
+              status: "CREATED", 
+              number: phase.phase, 
+              node: JSON.stringify(node), 
+              name: TaskRegistry[node.data.type].label
+            }
+          })
+        })
+      }
+    }, 
+    select: {
+      id: true, 
+      phases: true
+    },
+  });
+
+  if(!execution) {
+    throw new Error("workflow execution not created")
+  }
 }
