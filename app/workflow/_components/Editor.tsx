42cf005b111721813b30a040dcd42b64f8066faf
Christ Chadrak MVOUNGOU <ccmvoungou@gmail.com>
2025-04-24 17:12:30 +0200

    feat: Wrap Editor component in FlowValidationContextProvider for improved flow validation

--- app/workflow/_components/Editor.tsx
+++ app/workflow/_components/Editor.tsx
@@ -4,22 +4,25 @@ import { ReactFlowProvider } from "@xyflow/react"
 import FlowEditor from './FlowEditor';
 import TopBar from './topbar/TopBar';
 import TaskMenu from './TaskMenu';
+import { FlowValidationContextProvider } from '@/components/context/FlowValidationContext';
 
 function Editor({ workflow }: { workflow: Workflow }) {
   return (
-    <ReactFlowProvider>
-      <div className="flex flex-col h-full w-full">
-        <TopBar 
-          title="Workflow editor" 
-          subtitle={workflow.name} 
-          workflowId={workflow.id}
-        />
-        <section className='flex flex-1 h-full overflow-hidden'>
-          <TaskMenu />
-          <FlowEditor workflow={workflow} />
-        </section>
-      </div>
-    </ReactFlowProvider>
+    <FlowValidationContextProvider>
+      <ReactFlowProvider>
+        <div className="flex flex-col h-full w-full">
+          <TopBar 
+            title="Workflow editor" 
+            subtitle={workflow.name} 
+            workflowId={workflow.id}
+          />
+          <section className='flex flex-1 h-full overflow-hidden'>
+            <TaskMenu />
+            <FlowEditor workflow={workflow} />
+          </section>
+        </div>
+      </ReactFlowProvider>
+    </FlowValidationContextProvider>
   )
 }
 
